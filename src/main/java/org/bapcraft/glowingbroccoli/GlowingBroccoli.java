d7632c6941161e64cbfa77af27e59262576fd182
treyzania <treyzania@gmail.com>
2018-01-09 15:51:23 -0500

    Added main plugin file and gitignore.

---
+++ src/main/java/org/bapcraft/glowingbroccoli/GlowingBroccoli.java
@@ -0,0 +1,43 @@
+package org.bapcraft.glowingbroccoli;
+
+import org.slf4j.Logger;
+import org.spongepowered.api.Game;
+import org.spongepowered.api.config.DefaultConfig;
+import org.spongepowered.api.event.EventManager;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
+import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
+import org.spongepowered.api.plugin.Plugin;
+
+import com.google.inject.Inject;
+
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+
+@Plugin(id = "glowbroc", name = "GlowingBroccoli", version = "0.0.1")
+public class GlowingBroccoli {
+
+	@Inject
+	private Game game;
+	
+	@Inject
+	private EventManager eventManager;
+	
+	@Inject
+	private Logger logger;
+	
+	@Inject
+	@DefaultConfig(sharedRoot = true)
+	private ConfigurationLoader<CommentedConfigurationNode> configLoader;
+
+	@Listener
+	public void onPreInit(GamePreInitializationEvent event) {
+		
+	}
+	
+	@Listener
+	public void onInit(GameInitializationEvent event) {
+		this.logger.info("Congrats on your Glowing Broccoli!");
+	}
+	
+}
